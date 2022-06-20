aa7aa5a6587703719f8d10f217b8fd2878256c58
Obranen <krakhnytskyi@gmail.com>
2022-06-20 16:37:46 +0300

    init project

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './App.module.scss'
+
+function App() {
+  return (
+    <div className={classes.mainStyle}>
+      <h1>
+        Установить Redux
+      </h1>
+    </div>
+  );
+}
+
+export default App
